da93248511fef31295591bffe7bd66da556c0ed3
Sree <mavharsha@mavharsha.com>
2021-08-29 06:58:22 -0700

    fix

--- src/components/withdraw-modal/WithdrawModal.tsx
+++ src/components/withdraw-modal/WithdrawModal.tsx
@@ -26,7 +26,7 @@ function WithdrawModal(props: Props) {
   });
 
   useEffect(() => {
-    let accountType = props.creatorAddress === account ? 'Creator withdraw' : 'Fan withdraw';
+    let accountType = props.creatorAddress === account ? 'Creator' : 'Fan';
     setAccountType(accountType);
   }, [account, accountType, props.creatorAddress]) 
 
@@ -78,7 +78,6 @@ function WithdrawModal(props: Props) {
     <>
     <div className="fixed z-10 inset-0 overflow-y-auto" aria-labelledby="modal-title" role="dialog" aria-modal="true">
       <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-
         <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
         <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
 
@@ -87,7 +86,7 @@ function WithdrawModal(props: Props) {
             <div className="sm:flex sm:items-start">
               <div className="mt-3 text-center  sm:mt-0 sm:ml-4 sm:text-left">
                 <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
-                  {accountType === 'fan' ? `Withdrawing your eth from ${props.creator}'s pool` : `Withdrawing yeild for your pool`}
+                    {accountType === 'Fan' ? `Withdrawing your eth from ${props.creator}'s pool` : `Withdrawing yeild for your pool`}
                 </h3>
                   {!loading && 
                     <form className="mt-8 p-2 space-y-6" onSubmit={formik.handleSubmit}>
